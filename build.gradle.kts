254add427919abe875ac42842e6952ad9ad1fd13
thomas783 <thomas783789@gmail.com>
2025-02-05 11:56:41 +0900

    add settings for multi module

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,42 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
 plugins {
-    kotlin("jvm") version "1.9.23"
+  kotlin("jvm") version "2.1.10"
+  kotlin("plugin.serialization") version "2.1.10"
 }
 
-group = "com.kSerialization2Graphql"
-version = "1.0-SNAPSHOT"
-
-repositories {
+allprojects {
+  group = "com.kSerialization2Graphql"
+  repositories {
     mavenCentral()
+  }
 }
 
-dependencies {
-    testImplementation(kotlin("test"))
-}
+subprojects {
+  apply { plugin("kotlin") }
+  dependencies {
+    // reflection
+    implementation(kotlin("reflect"))
 
-tasks.test {
-    useJUnitPlatform()
-}
-kotlin {
+    // test
+    testImplementation("io.kotest:kotest-runner-junit5:5.8.0")
+    testImplementation("org.assertj:assertj-core:3.25.3")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
+  }
+
+  tasks {
+    test {
+      useJUnitPlatform()
+      testLogging {
+        events = setOf(TestLogEvent.FAILED, TestLogEvent.SKIPPED, TestLogEvent.PASSED)
+        showStandardStreams = true
+        exceptionFormat = TestExceptionFormat.FULL
+      }
+    }
+  }
+
+  kotlin {
     jvmToolchain(21)
+  }
 }
---
+++ kotlin-serialization-to-graphql-common/build.gradle.kts
@@ -0,0 +1,9 @@
+dependencies {
+    // reflection
+    implementation(kotlin("reflect"))
+
+    // test
+    testImplementation("io.kotest:kotest-runner-junit5:5.8.0")
+    testImplementation("org.assertj:assertj-core:3.25.3")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
+}
